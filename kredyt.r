126332fa121b2eefd02aa41b612224c9030c3d62
Maciej Rydzy <41112526+MaciejRydzy@users.noreply.github.com>
2020-12-31 08:39:23 +0100

    wygladzenie zmiennych

--- kredyt.r
+++ kredyt.r
@@ -1,5 +1,22 @@
 library(FinancialMath)
 
+# wektor liczby dni w miesiacu: rok zwykly
+dni_w_roku <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+# wektor liczby dni w miesiacu: rok przestepny
+dni_w_roku_p <- dni_w_roku
+dni_w_roku_p[2] <- 29
+
+# wektor liczby dni w miesiacu dla kolejnej raty
+wek_dni_w_mies <- c(dni_w_roku[9:12], rep(c(dni_w_roku, dni_w_roku_p, dni_w_roku, dni_w_roku), length.out=355))
+# wektor liczby dni w roku dla kolejnej raty
+wek_dni_w_roku <- c(rep(365, times=4), rep(rep(c(365,366,365,365), each=12), length.out=355))
+
+# wektory z ustalonym przez mBank poziomem oprocentowania oraz wysokoscia pobranych rat poczawszy od raty #3
+oprocentowanie_raty_mB <- rep(c(3.05, 3.35, 3.65, 3.95, 3.70, 3.55, 3.25, 2.35), times=c(3,2,6,20,10,6,55,70))
+raty_pobrane_mB <- rep(c(306.84, 306.83, 306.84, 318.53, 330.45, 342.35, 332.86, 327.42, 316.68, 290.48), times=c(1,1,1,2,6,20,10,6,55,70))
+
+
 
 
 ##########################################
@@ -12,6 +29,7 @@ oprocentowanie <- 3.45
 poz_lba_rat <- 359
 dni_w_mcu <- c("cze" = 23, "lip" = 31)
 ##########################################
+cat("-----------  rata #1  -----------\n")
 
 # wyliczenie raty kapitalowo-odsetkowej wylacznie w miesiacu lipcu
 oprocentowanie <- oprocentowanie / 100
@@ -35,16 +53,18 @@ harmonogram <- data.frame(rata = rata_calkowita + round(as.vector(rata_odsetkowa
 
 
 
+
 ##########################################
 #
 #               rata #2
 #
 ##########################################
 oprocentowanie <- c(3.45,2.45)
-oprocentowanie_mB <- 3.75
-dni_oprocentowania <- c(1,30) # jeden dzień z oprocentowaniem 3,45% oraz 30 dni z oprocentowaniem 2,45%
+oprocentowanie_mB <- 3.75       # oprocentowanie ustalone przez mBank
+dni_oprocentowania <- c(1,30)   # jeden dzień z oprocentowaniem 3,45% oraz 30 dni z oprocentowaniem 2,45%
 poz_lba_rat <- 358
 ##########################################
+cat("-----------  rata #2  -----------\n")
 
 # wyliczenie raty calkowitej
 oprocentowanie <- oprocentowanie / 100
@@ -86,38 +106,25 @@ harmonogram <- rbind(harmonogram, data.frame(rata = rata_calkowita[], kapital = 
 oprocentowanie = 2.45
 poz_lba_rat <- 357
 ##########################################
-# wektor liczby dni w miesiacu: rok zwykly
-dni_w_roku <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-
-# wektor liczby dni w miesiacu: rok przestepny
-dni_w_roku_p <- dni_w_roku
-dni_w_roku_p[2] <- 29
-
-# wektor liczby dni w miesiacu dla danej raty
-wek_dni_w_mies <- c(dni_w_roku[9:12], rep(c(dni_w_roku, dni_w_roku_p, dni_w_roku, dni_w_roku), times = 7), dni_w_roku, dni_w_roku_p[1:5])
-# wektor liczby dni w roku dla danej raty
-wek_dni_w_roku <- c(rep(365, times = 4), rep(c(rep(365, times = 12), rep(366, times = 12), rep(365, times = 12), rep(365, times = 12)), times = 7), rep(365, times = 12), rep(366, times = 5))
-
-# wektory z ustalonym przez mBank poziomem oprocentowania oraz wysokoscia pobranych rat poczawszy od raty #3
-oprocentowanie_raty <- rep(c(3.05, 3.35, 3.65, 3.95, 3.70, 3.55, 3.25, 2.35), times=c(3,2,6,20,10,6,55,70))
-raty_pobrane <- rep(c(306.84, 306.83, 306.84, 318.53, 330.45, 342.35, 332.86, 327.42, 316.68, 290.48), times=c(1,1,1,2,6,20,10,6,55,70))
-##########################################
+cat("----------  rata #3 +  ----------\n")
 
 # wyliczenie raty calkowitej
 oprocentowanie <- oprocentowanie / 100
 rata_calkowita <- amort.period(Loan = kapital, n = poz_lba_rat, i = oprocentowanie, ic = 12, pf = 12)
 rata_calkowita <- round(rata_calkowita[2], digits = 2)
-cat("rata_calkowita =", rata_calkowita, "\n")
+cat("rata_calkowita =", rata_calkowita, "\n\n")
 
 # wyliczenie pozostalej czesci harmonogramu
-for(rata in 1:length(wek_dni_w_mies)) { 
+for(rata in 1:length(wek_dni_w_mies)) {
   rata_odsetkowa <- round(kapital * oprocentowanie * wek_dni_w_mies[rata] / wek_dni_w_roku[rata], digits = 2)
   rata_kapitalowa <- rata_calkowita - rata_odsetkowa
   kapital <- kapital - rata_kapitalowa
-  
-  harmonogram <- rbind(harmonogram, data.frame(rata = rata_calkowita, kapital = rata_kapitalowa, odsetki = rata_odsetkowa, kapital_do_splaty = kapital, nadplata = raty_pobrane[rata] - rata_calkowita))
+
+  harmonogram <- rbind(harmonogram, data.frame(rata = rata_calkowita, kapital = rata_kapitalowa, odsetki = rata_odsetkowa, kapital_do_splaty = kapital, nadplata = raty_pobrane_mB[rata] - rata_calkowita))
 }
 
-splacony_kapital <- sum(harmonogram[1:174,1])
+cat("-----  stan na 01.01.2021  -----\n")
+splacony_kapital <- sum(harmonogram[1:174,2])
+kapital_do_splaty <- kapital_poczatkowy - splacony_kapital
 cat("splacony_kapital =", splacony_kapital, "\n")
-cat("kapital_do_splaty =", kapital_poczatkowy - splacony_kapital, "\n")
+cat("kapital_do_splaty =", kapital_do_splaty, "\n")
